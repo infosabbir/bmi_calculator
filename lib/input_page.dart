69a6e5dfb818e937eebf28ea68861b924321104a
infosabbir <infosabbir9@gmail.com>
2026-01-13 21:50:24 +0600

    first commit

---
+++ lib/input_page.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  const InputPage({super.key});
+
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    final colour = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
+
+    return Scaffold(
+      backgroundColor: colour.surface,
+      appBar: AppBar(
+        backgroundColor: colour.surfaceContainer,
+        centerTitle: true,
+        title: Text(
+          'BMI CALCULATOR',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(child: ReusableCard(colour: colour)),
+                Expanded(child: ReusableCard(colour: colour)),
+              ],
+            ),
+          ),
+          Expanded(child: ReusableCard(colour: colour)),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(child: ReusableCard(colour: colour)),
+                Expanded(child: ReusableCard(colour: colour)),
+              ],
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: colour.primary,
+            width: double.infinity,
+            height: 80,
+            child: Text(
+              'CALCULATE',
+              style: textTheme.displaySmall!.copyWith(
+                color: colour.onPrimary,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  const ReusableCard({super.key, required this.colour});
+
+  final ColorScheme colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(12.0),
+
+      decoration: BoxDecoration(
+        color: colour.surfaceContainerLow,
+        borderRadius: BorderRadius.circular(12),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:bmi_calculator/input_page.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      builder: (context) {
+        return const MyApp();
+      },
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      theme: ThemeData.from(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.teal,
+          brightness: Brightness.dark,
+        ),
+      ),
+      home: InputPage(),
+    );
+  }
+}
